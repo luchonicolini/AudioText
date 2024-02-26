74d51bebb0cb2587d95a52e6a7c16acdcb2f9d79
luciano <nicolini.luciano@hotmail.com>
2024-02-26 18:42:19 -0300

    VERSION 2

--- AudioText/Views/AudioTextView.swift
+++ AudioText/Views/AudioTextView.swift
@@ -11,75 +11,59 @@ import AVKit
 struct AudioTextView: View {
     @StateObject private var transcriber = AudioTranscriber()
     @State private var isRecording = false
-    @State private var showError = false
-    @State private var audioPlayer: AVPlayer? // Añade un reproductor de audio
 
     var body: some View {
-        NavigationView {
-            VStack {
+        VStack {
+            HStack {
                 if transcriber.isAuthorized {
-                    Image(systemName: isRecording ? "mic.fill" : "mic.slash.fill")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 100)
-                        .foregroundColor(isRecording ? .blue : .red)
-                        .onTapGesture {
-                            isRecording.toggle()
-                            if isRecording {
-                                transcriber.startTranscribing()
-                            } else {
-                                transcriber.stopTranscribing()
-                            }
-                        }
-
-                    // Añade un botón de reproducción
                     Button(action: {
-                        if let audioURL = transcriber.audioURL {
-                            audioPlayer = AVPlayer(url: audioURL)
-                            audioPlayer?.play()
+                        if isRecording {
+                            transcriber.stopTranscribing()
+                        } else {
+                            transcriber.startTranscribing()
                         }
+                        isRecording.toggle()
                     }) {
-                        Image(systemName: "play.fill")
+                        Image(systemName: isRecording ? "stop.circle.fill" : "mic.fill")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
                             .frame(width: 50)
-                            .foregroundColor(.blue)
                     }
-
-                    ScrollView {
-                        Text(transcriber.transcription)
-                            .padding()
-                            .background(Color(.systemGray6))
-                            .cornerRadius(10)
-                            .padding()
-                    }
-                    .frame(height: 200)
                 } else {
                     Text("El reconocimiento de voz no está autorizado")
                         .foregroundColor(.red)
                 }
+
+                Spacer()
+
+                if isRecording {
+                    Button(action: {
+                        transcriber.stopTranscribing() // Stop button
+                        isRecording = false
+                    }) {
+                        Text("Detener")
+                    }
+                }
             }
-            .onAppear(perform: transcriber.requestSpeechAuthorization)
-            .alert(isPresented: $showError) {
-                Alert(title: Text("Error"), message: Text(transcriber.error ?? ""), dismissButton: .default(Text("OK")) {
-                    transcriber.error = nil
-                })
+
+            if isRecording {
+                Text("Transcribiendo...")
+                    .foregroundColor(.gray)
             }
-            .onChange(of: transcriber.error) { oldValue, newValue in
-                showError = transcriber.error != nil
+
+            ScrollView {
+                Text(transcriber.transcription)
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10)
+                    .padding()
+                    .lineLimit(nil) // Remove line limit to accommodate long transcriptions
             }
-            .navigationBarTitle("Transcriptor de Audio", displayMode: .inline)
-            .navigationBarItems(trailing:
-                                    Button(action: {
-                // Aquí debes implementar la lógica para importar o exportar archivos de audio
-            }) {
-                Image(systemName: "square.and.arrow.down")
-            })
         }
+        .onAppear(perform: transcriber.requestSpeechAuthorization)
+       
     }
 }
-
-
 
 
 
--- AudioText/Views/AudioTranscriber.swift
+++ AudioText/Views/AudioTranscriber.swift
@@ -7,53 +7,96 @@
 
 import Foundation
 import Speech
+import SwiftUI
+
 
 class AudioTranscriber: ObservableObject {
-    @Published var isAuthorized = false
-    @Published var transcription = ""
-    @Published var error: String?
-    
-    private var recognitionTask: SFSpeechRecognitionTask? // Añade una variable para la tarea de reconocimiento
+  @Published var isAuthorized = false
+  @Published var transcription = ""
+  @Published var error: String?
+  @State private var isRecording = false
 
-    func requestSpeechAuthorization() {
-        SFSpeechRecognizer.requestAuthorization { authStatus in
-            DispatchQueue.main.async {
-                switch authStatus {
-                case .authorized:
-                    self.isAuthorized = true
-                default:
-                    self.isAuthorized = false
-                }
-            }
-        }
+  private var recognitionTask: SFSpeechRecognitionTask?
+  private let audioEngine = AVAudioEngine()
+  private let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer(locale: Locale(identifier: "es-ES"))
+  private let request = SFSpeechAudioBufferRecognitionRequest()
+  private var audioRecorder: AVAudioRecorder?
+
+  var audioURL: URL? {
+    audioRecorder?.url
+  }
+
+  func requestSpeechAuthorization() {
+    SFSpeechRecognizer.requestAuthorization { authStatus in
+      DispatchQueue.main.async {
+        self.isAuthorized = authStatus == .authorized
+      }
+    }
+  }
+
+  func startTranscribing() {
+    guard let recognizer = speechRecognizer, recognizer.isAvailable else {
+      self.error = "El reconocimiento de voz no está disponible"
+      return
     }
 
-    func transcribeAudio() {
-        // Aquí debes proporcionar la URL del archivo de audio que deseas transcribir
-        guard let audioURL = Bundle.main.url(forResource: "lucho", withExtension: "m4a") else {
-            self.error = "No se pudo encontrar el archivo de audio"
-            return
-        }
+    do {
+      let inputNode = audioEngine.inputNode
+      let recordingFormat = inputNode.outputFormat(forBus: 0)
+      inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { [weak self] (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+        self?.request.append(buffer)
+      }
 
-        // Establece el locale del reconocedor a español
-        let recognizer = SFSpeechRecognizer(locale: Locale(identifier: "es-ES"))
-        let request = SFSpeechURLRecognitionRequest(url: audioURL)
+      audioEngine.prepare()
+      try audioEngine.start()
 
-        recognitionTask = recognizer?.recognitionTask(with: request) { (result, error) in
-            if let error = error {
-                self.error = "Hubo un error: \(error.localizedDescription)"
-            } else if let result = result {
-                self.transcription = result.bestTranscription.formattedString
-            }
+      recognitionTask = recognizer.recognitionTask(with: request) { [weak self] (result, error) in
+        guard let self = self else { return }
+        if let error = error {
+          self.error = "Hubo un error: \(error.localizedDescription)"
+        } else if let result = result {
+          self.transcription = result.bestTranscription.formattedString
         }
+      }
+
+      // Inicia la grabación de audio
+      let audioFilename = self.getDocumentsDirectory().appendingPathComponent("recording.m4a")
+      let settings: [String: Any] = [
+        AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+        AVSampleRateKey: 44100,
+        AVNumberOfChannelsKey: 1,
+        AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+      ]
+      audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+      audioRecorder?.record()
+    } catch {
+      self.error = "Hubo un error: \(error.localizedDescription)"
     }
-    
-    func stopTranscribing() {
-        recognitionTask?.cancel() // Detiene la tarea de reconocimiento
-        recognitionTask = nil
-        transcription = "" // Restablece la transcripción a cero
-    }
+  }
+
+  func stopTranscribing() {
+    audioEngine.stop()
+    audioEngine.inputNode.removeTap(onBus: 0)
+    recognitionTask?.cancel()
+    recognitionTask = nil
+    request.endAudio()
+    transcription = "" // Restablece la transcripción a cero
+
+    // Detiene la grabación de audio
+    audioRecorder?.stop()
+  }
+
+  func cancelTranscribing() {
+    isRecording = false
+    stopTranscribing()
+  }
+
+  private func getDocumentsDirectory() -> URL {
+    FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+  }
 }
+
+
 
 
 
